327fba2ce35649e8743bf03399afcef40cf7e9c9
Ahmed Dawood <ahmeddawood666680@gmail.com>
2021-12-30 03:10:22 +0200

    trying to solve the block dying problem

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ pygame.init()
 SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 CLOCK = pygame.time.Clock()
 SCREEN.fill(BLACK)
-
+blockSize = 20
 
 def drawGrid():
     blockSize = 20 #Set the size of the grid block
@@ -27,10 +27,27 @@ def drawRect(x,y,i=0,color=WHITE):
     rect = pygame.Rect(x, y, blockSize, blockSize)
     pygame.draw.rect(SCREEN, color, rect, i)            # 0 is for a solid full colored rectangle , 1 is for hollow rectangle (border only)
 
+def getNeighbors(rect):     # rect is a tuple of x,y coordinates for the rectangle
+    neighbors=[]
+    x,y=rect
+    if y-blockSize>=0:neighbors.append((x,y-blockSize))
+    if y+blockSize<WINDOW_HEIGHT:neighbors.append((x,y+blockSize))
+    if x-blockSize>=0:neighbors.append((x-blockSize,y))
+    if x+blockSize<WINDOW_WIDTH:neighbors.append((x+blockSize,y))
+
+    if x-blockSize>=0 and y-blockSize>=0: neighbors.append((x-blockSize,y-blockSize))
+    if x+blockSize<WINDOW_WIDTH and y-blockSize>=0: neighbors.append((x+blockSize,y-blockSize))
+    if x - blockSize >= 0 and y + blockSize < WINDOW_HEIGHT: neighbors.append((x - blockSize, y + blockSize))
+    if x + blockSize < WINDOW_WIDTH and y + blockSize < WINDOW_HEIGHT: neighbors.append((x + blockSize, y + blockSize))
+
+    return neighbors
+
+
 white_rects=[]
 black_rects=[]                  # rectangles returning to black after being pressed while white
 
-
+birth=[]
+die=[]
 
 while True:
     drawGrid()
@@ -44,7 +61,12 @@ while True:
 
         # mouse pos if clicked
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
+
+            print("----")
+            print(white_rects)
+            print(black_rects)
+
+            # print(pygame.mouse.get_pos())
             x,y=pygame.mouse.get_pos()
             x=(x//20)*20
             y=(y//20)*20
@@ -57,9 +79,27 @@ while True:
                 black_rects.append((x, y))
                 white_rects.remove((x,y))
 
-        # if event.type == pygame.KEYDOWN:                # next step in the game (next state)
-        #     if event.key == pygame.K_SPACE:
-        #
+        if event.type == pygame.KEYUP:                # next step in the game (next state)
+            if event.key == pygame.K_SPACE:
+                for ii in white_rects:
+                    die=[]
+                    cnt=0
+                    l=getNeighbors(ii)
+                    for i in l:
+                        if i in white_rects:
+                            cnt+=1
+                    if cnt<2:
+                        die.append(ii)
+                    for x, y in die:
+                        drawRect(x, y)
+
+                        # if ii not in black_rects:black_rects.append(ii)
+                        # else:black_rects.remove(ii)
+                        # try:white_rects.remove(ii)
+                        # except:pass
+
+
+
 
 
 
@@ -70,8 +110,13 @@ while True:
     for x, y in black_rects:                # redrawing the black rectangle in the repressed rectangle
         drawRect(x, y,0,BLACK)
         drawRect(x, y,1)
+    for x,y in white_rects:                 # removing the stuck blocks
+        if (x,y) in black_rects:
+            black_rects.remove((x,y))
+
 
     pygame.display.update()
+
 
 
 
